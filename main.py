71fbd39a7b0b9cb47e075ed1e53ec8a9a5f72252
Sumon Chhetry <sumonchetry1@gmail.com>
2022-10-15 16:19:58 +0530

    slot machine logic

--- main.py
+++ main.py
@@ -4,6 +4,31 @@ MAX_LINES = 3
 MAX_BET = 300
 MIN_BET = 1
 
+ROWS = 3
+COLS = 3
+
+symbol_count = {
+    'A': 2,
+    'B': 4,
+    'C': 6,
+    'D': 8
+}
+
+def get_slot_machine_spin(rows, cols, symbols):
+    all_symbols = []
+    for symbol, symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+    columns = []
+    for col in range(cols):
+        column = []
+        current_symbols = all_symbols[:]
+        for row in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+        columns.append(column)
+    return columns
 
 def deposit():
     while True:
@@ -59,7 +84,7 @@ def main():
             print(f"You do not have enough to bet that amount, Your current balance is: ${balance}")
         else:
             break
-    print(f"You are better ${bet} on ${lines} lines. Total bet is equal to ${total_bet}")
+    print(f"You are betting ₹{bet} on {lines} lines. Total bet is equal to ₹{total_bet}")
 
 
 main()
